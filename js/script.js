217de15f46d9d8a09b625c06c73e45476b81a832
Arman Z. Papyan <Arman.Z.Papyan@ad.ucom.am>
2022-10-16 22:46:15 +0400

    add t function for edit month

--- js/script.js
+++ js/script.js
@@ -1,24 +1,40 @@
 const currentDate = document.querySelector('.current-data')
 const daysTag = document.querySelector('.days')
-
-// getting new date,    Year and Month
+const prevNextIcon = document.querySelectorAll('.icons span')
 let date = new Date(),
     currYear = date.getFullYear(),
     currMonth = date.getMonth();
-// console.log('data-->',date)
-// console.log('currYear-->',currYear)
-// console.log('currMonth-->',currMonth)
 const Month = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
 const renderCalendar = ()=>{
+    let firstDayofMonth = new Date(currYear,currMonth+1).getDate()
     let lastDateOfMonth = new Date(currYear,currMonth+1, 0).getDate()
     let liTag = ''
+
+    for(leti =firstDayofMonth;i<lastDateOfMonth ; i++){
+
+    }
     console.log(lastDateOfMonth)
     for(let i=1; i<=lastDateOfMonth;i++ ){
         liTag += `<li>${i}</li>`
-        console.log(i)
     }
     currentDate.innerText = `${Month[currMonth]} ${currYear}`
     daysTag.innerHTML = liTag
 }
 renderCalendar()
+
+prevNextIcon.forEach(icon =>{
+   icon.addEventListener('click',()=>{
+    currMonth = icon.id === "next"? currMonth + 1 : currMonth - 1
+    if(currMonth>11){
+        currMonth = 0
+        currYear=currYear+1
+    }else if (currMonth<-1){
+        currYear=currYear-1
+        currMonth = 11
+    }
+    renderCalendar()
+    return 0
+   })
+})
+
