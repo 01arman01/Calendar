723423e58a9b25d769140d10cf7a5c59785724f9
Arman <papyandeveloper@gmail.com>
2022-10-17 01:28:24 +0400

    end calendar code

--- js/script.js
+++ js/script.js
@@ -7,16 +7,23 @@ let date = new Date(),
 const Month = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
 const renderCalendar = ()=>{
-    let firstDayofMonth = new Date(currYear,currMonth+1).getDate()
+    let firstDayofMonth = new Date(currYear,currMonth,1).getDay()
     let lastDateOfMonth = new Date(currYear,currMonth+1, 0).getDate()
+    let lastDayOfMonth = new Date(currYear,currMonth, lastDateOfMonth).getDay()
+    let lastDateOfLastMonth = new Date(currYear,currMonth , 0).getDate()
     let liTag = ''
 
-    for(leti =firstDayofMonth;i<lastDateOfMonth ; i++){
+    for(let i =firstDayofMonth;i>0; i--){
 
+        liTag += `<li class="inactive">${lastDateOfLastMonth- i+1}</li>`
     }
-    console.log(lastDateOfMonth)
+
     for(let i=1; i<=lastDateOfMonth;i++ ){
-        liTag += `<li>${i}</li>`
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear()? "active" : ""
+        liTag += `<li class="${isToday}">${i}</li>`
+    }
+    for(let i=lastDayOfMonth; i<6;i++ ){
+        liTag += `<li class="inactive">${i -lastDayOfMonth+1}</li>`
     }
     currentDate.innerText = `${Month[currMonth]} ${currYear}`
     daysTag.innerHTML = liTag
@@ -26,12 +33,12 @@ renderCalendar()
 prevNextIcon.forEach(icon =>{
    icon.addEventListener('click',()=>{
     currMonth = icon.id === "next"? currMonth + 1 : currMonth - 1
-    if(currMonth>11){
-        currMonth = 0
-        currYear=currYear+1
-    }else if (currMonth<-1){
-        currYear=currYear-1
-        currMonth = 11
+    if(currMonth <0 || currMonth > 11){
+        date = new  Date(currYear, currMonth)
+        currYear = date.getFullYear()
+        currMonth = date.getMonth()
+    }else {
+        date = new  Date()
     }
     renderCalendar()
     return 0
